5af07436d261d8764ac360ee30ef371420e62e5b
Susana <susyvalencia122@gmail.com>
2024-10-23 16:10:54 -0500

    se crea controlador paciente

---
+++ src/controllers/controladorpaciente.js
@@ -0,0 +1,17 @@
+// OBJETIVO: Capturar los datos de un formulario
+
+// PASO 1. Por cada input, select, textarea del formulario
+//se crea una variable 
+
+//PASO 2. Por cada variable debo utilizar el DOM para 
+//asociar el html con js
+
+let nombrePciente=document.getElementById("nombrepaciente")
+let correoPaciente=document.getElementById("correopaciente") 
+let telefonoPaciente=document.getElementById("telefonopaciente")
+let fechaNacimientoPaciente=document.getElementById("nacimientopaciente")
+let ciudadPaciente=document.getElementById("ciudadpaciente")
+let polizaPaciente=document.getElementById("polizapaciente")
+let ipsPaciente=document.getElementById("ipspaciente")
+let grupoIngresoPaciente=document.getElementById("ingresospaciente")
+let fechaAfiliacionPaciente=document.getElementById("afiliacionpaciente")
